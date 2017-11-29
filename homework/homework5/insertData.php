27ed6d2c587c21d00a50cb5b0f212cba8b33d801
Daniel Deleon <ddeleon@csumb.edu>
2017-11-29 18:58:43 +0000

    Homework 5 update

--- homework/homework5/insertData.php
+++ homework/homework5/insertData.php
@@ -2,19 +2,17 @@
  include("../../dbConnection.php");
  $conn = getDatabaseConnection();
 
- if(isset($_GET('city'))){
+ if(isset($_GET['city'])){
    $sql = "INSERT INTO tc_seach
             (City)
             VALUES
             (:city)";
             $namedParameters[':city'] =  $_GET['city'];
-            printf("Bye1");
 
  }
- 
  else{
    $sql = "INSERT INTO tc_seach
-   (Zip)
+   (Zipcode)
    VALUES
    (:zip)";
    $namedParameters[':zip'] =  $_GET['zip'];
